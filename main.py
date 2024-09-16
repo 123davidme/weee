6ec8b10c79d484b4dbf250435dbc7370e4ade0b7
Oluseye Lamina <oluseyelam@gmail.com>
2024-09-16 12:09:13 -0700

    first commit -committing all files to the repository

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-.ideal/
+def bio_data():
+    first_name = input("Enter first name: ")
+    second_name = input("Enter second name")
+    gender = input("Enter gender: ")
+    city = input("Enter city: ")
+    country = input("Enter country")
+    course_study = input('enter course study')
+    return f"My name is  {first_name} {second_name}, I am a {gender} and leave in {city} {country}. I am in Pora Academy to study {course_study}"
+
+print(bio_data())
